78664a631dd4c830aacedd6395e9c936f2ffe94a
tasnimmabrouk <156331930+Tasnimtass@users.noreply.github.com>
2025-01-10 16:16:49 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+const App=()=> {
+  const [message, setMessage] = useState('');// Stocke la réponse de l'API
+  useEffect(() => {
+    // Requête vers le backend via le proxy
+    fetch('/api/message')
+      .then(response => response.json())//convertit la reponse en json
+      .then((data) => setMessage(data.message))//les donnes de json{salut depuis backend} sont afficher 
+      .catch((error) => console.error('Erreur lors de la récupération des données:', error));
+  }, []); 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{message || 'Chargement ...'}</p> 
+      </header>
+    </div>
+  );
+}
+export default App;
